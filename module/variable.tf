bf5c81bad9f1a20e69a57a231ce14ef0678fb20c
Mohan Kumar <mohan41204@gmail.com>
2026-04-28 21:57:15 +0530

    updating variable.tf file

--- module/variable.tf
+++ module/variable.tf
@@ -1,15 +1,19 @@
 variable "s3" {
   description = "s3 bucket name"
+  type = string
 }
 
 variable "acl" {
   description = "acl"
+  type = bool
 }
 
 variable "file" {
   description = "uplading file"
+  type = string
 }
 
 variable "path" {
   description = "path"
+  type = string
 }
--- variable.tf
+++ variable.tf
@@ -1,19 +1,23 @@
 variable "s3" {
   description = "s3 bucket name"
+  type = string
   default = "staticsite280426"
 }
 
 variable "acl" {
   description = "acl"
+  type = bool
   default = false
 }
 
 variable "file" {
   description = "uplading file"
+  type = string
   default = "index.html"
 }
 
 variable "path" {
   description = "path"
+  type = string
   default = "./WebSite/index.html"
 }
